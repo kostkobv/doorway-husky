6c8e58aeae7d48933843c71a0f36b1a2f3aadad5
Bohdan Kostko <bohdan.kostko@nopattern.net>
2016-01-30 13:54:46 +0100

    chore(setup): description and install requires

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     name='doorway-husky',
     version='0.0.1',
     packages=[''],
+    install_requires=['python-linkedin'],
     url='https://github.com/kostkobv/doorway-husky',
     license='Apache 2.0',
     author='bv',
     author_email='bohdan.kostko@gmail.com',
-    description=''
+    description='Checks data on linkedin from provided .xlsx file and corrects if it is not so.'
 )
