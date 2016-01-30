1c6e0c18479f279ea0a93e98aaad6701d14cd244
Bohdan Kostko <bohdan.kostko@nopattern.net>
2016-01-30 13:33:06 +0100

    feat(conf): create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='doorway-husky',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/kostkobv/doorway-husky',
+    license='Apache 2.0',
+    author='bv',
+    author_email='bohdan.kostko@gmail.com',
+    description=''
+)
